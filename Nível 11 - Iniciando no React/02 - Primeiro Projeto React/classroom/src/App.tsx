11a5ff8114e159b69d6b742a2a40aeaa198aaee8
Paulo-Alvares <paulo.alvares@sptech.school>
2025-11-02 22:16:12 -0300

    [chore]: init adivinhe project with React

---
+++ Nível 11 - Iniciando no React/02 - Primeiro Projeto React/classroom/src/App.tsx
@@ -0,0 +1,38 @@
+import styles from "./app.module.css";
+
+import { Tip } from "./components/Tip";
+import { Input } from "./components/Input";
+import { Letter } from "./components/Letter";
+import { Header } from "./components/Header";
+
+function App() {
+  function handleRestartGame() {
+    alert("Reiniciar o jogo");
+  }
+
+  return (
+    <div className={styles.container}>
+      <main>
+        <Header current={5} max={10} onRestart={handleRestartGame} />
+
+        <Tip tip="Uma das linguagens de programação mais utilizadas" />
+
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div>
+          <Input autoFocus maxLength={1} placeholder="?" />
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;

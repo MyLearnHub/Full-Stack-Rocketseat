527d506fb6a5b80e026b6f510755aed71aeb5bd2
Paulo-Alvares <paulo.alvares@sptech.school>
2025-11-15 13:34:14 -0300

    [doc]: finishing http apis with react project

--- Nível 12 - Avançando no React/04 - Requisições HTTP e API/web/src/pages/Refund.tsx
+++ Nível 12 - Avançando no React/04 - Requisições HTTP e API/web/src/pages/Refund.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
 import { z, ZodError } from "zod";
 import { AxiosError } from "axios";
@@ -6,6 +6,7 @@ import { AxiosError } from "axios";
 import { api } from "../services/api";
 import fileSvg from "../assets/file.svg";
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
+import { formatCurrency } from "../utils/formatCurrency";
 
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
@@ -28,6 +29,7 @@ export function Refund() {
   const [category, setCategory] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [file, setFile] = useState<File | null>(null);
+  const [fileUrl, setFileUrl] = useState<string | null>(null);
 
   const navigate = useNavigate();
   const params = useParams<{ id: string }>();
@@ -80,6 +82,31 @@ export function Refund() {
     }
   }
 
+  async function fetchRefund(id: string) {
+    try {
+      const { data } = await api.get<RefundAPIResponse>(`/refunds/${id}`);
+
+      setName(data.name);
+      setCategory(data.category);
+      setAmount(formatCurrency(data.amount));
+      setFileUrl(data.filename);
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message);
+      }
+
+      alert("Não foi possivel carregar");
+    }
+  }
+
+  useEffect(() => {
+    if (params.id) {
+      fetchRefund(params.id);
+    }
+  }, [params.id]);
+
   return (
     <form
       onSubmit={onSubmit}
@@ -127,9 +154,9 @@ export function Refund() {
         />
       </div>
 
-      {params.id ? (
+      {params.id && fileUrl ? (
         <a
-          href="https://www.rocketseat.com.br"
+          href={`http://localhost:3333/uploads/${fileUrl}`}
           target="_blank"
           className="text-sm text-green-100 font-semibold flex items-center justify-center gap-2 my-6 hover:opacity-70 transition ease-linear"
         >
